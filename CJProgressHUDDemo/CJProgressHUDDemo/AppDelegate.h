9515ad928129ea3e39dba08ace18f6ec8fdca236
J.Cheng <1875214064@qq.com>
2017-07-26 14:36:10 +0800

    version 1.0.0

---
+++ CJProgressHUDDemo/CJProgressHUDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJProgressHUDDemo
+//
+//  Created by 北极星电力 on 2017/7/26.
+//  Copyright © 2017年 北极星电力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CJProgressHUDDemo/CJProgressHUDDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJProgressHUDDemo
+//
+//  Created by 北极星电力 on 2017/7/26.
+//  Copyright © 2017年 北极星电力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
